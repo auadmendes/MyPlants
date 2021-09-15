b618fe3bfc00a4ee0f4e4f2c0263c52e093fbe78
Luciano Mendes <luciano.auad@gmail.com>
2021-09-14 21:24:28 -0300

    Update firebaseconnection.js

--- src/services/firebaseconnection.js
+++ src/services/firebaseconnection.js
@@ -4,14 +4,14 @@ import 'firebase/auth';
 import 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD6t5d9vRdVYKT1q6AyMZVLeqkJHdbzvRs",
-    authDomain: "meuapp-992a1.firebaseapp.com",
-    databaseURL: "https://meuapp-992a1-default-rtdb.firebaseio.com",
-    projectId: "meuapp-992a1",
-    storageBucket: "meuapp-992a1.appspot.com",
-    messagingSenderId: "346038815493",
-    appId: "1:346038815493:web:b10c0424e5fbef9bee856c",
-    measurementId: "G-4X3K4WFS9V"
+    apiKey: "yourapi",
+    authDomain: "Replace",
+    databaseURL: "hReplace",
+    projectId: "Replace",
+    storageBucket: "Replace",
+    messagingSenderId: "Replace",
+    appId: "Replace",
+    measurementId: "Replace"
 };
 
 if (!firebase.apps.length) {
